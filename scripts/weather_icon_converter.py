bbaf3c5844dad107518f570e7ec6e1d4fff7dc86
Jeff <abc@abc.com>
2019-01-01 11:49:49 -0500

    Weather icon converter script works.

--- scripts/weather_icon_converter.py
+++ scripts/weather_icon_converter.py
@@ -1,28 +1,46 @@
+# weather_icon_converter.py
+#
+# Converts the svg weather images to png.
+
 
 import os
-from os import listdir
-import subprocess 
+import argparse
+import subprocess
 
 
-def processFile(filePath):
-    # print("Number of lines in {}".format(filePath))
-    subprocess.call(["wc", "-l", filePath])
+def processFile(dirName, fileName):
+    baseName, fileExtension = fileName.split(".")
 
-def processFiles():
-    files = os.listdir()
+    svgFile = fileName
+    pngFile = "{}.png".format(baseName)
+
+    svgPath = os.path.join(dirName, fileName)
+    pngPath = os.path.join(dirName, pngFile)
+
+    print("Converting {} to {}".format(svgPath, pngPath))
+
+    try:
+        subprocess.check_output(["C:/Program Files/Inkscape/inkscape", "-z", "-e", pngPath, "-w" "200", "-h", "200", svgPath])
+
+    except subprocess.CalledProcessError as err:
+        print("Error converting {}: {}".format(svgFile, err.output))
+
+
+def processFiles(inDirectory):
+    files = os.listdir(inDirectory)
     print("Files:")
     for file in files:
-        # print("  {}".format(file))
-        processFile(file)
+        processFile(inDirectory, file)
 
 
 # ---------------------------------------------------------------
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("-w", "--windowed", action="store_true", default=False, dest="windowed", help="Use windowed mode instead of full-screen")
-    # parser.add_argument("--no_backlight", action="store_true", default=False, dest="no_backlight", help="Disable backlight control")
+    parser = argparse.ArgumentParser(description='Convert weather icons from SVG to PNG.')
+
+    parser.add_argument("directory", type=str, help="Directory of SVG files")
 
 
-    # args = parser.parse_args()
+    args = parser.parse_args()
 
-    processFiles()
+    print("Processing icons in: {}".format(args.directory))
+    processFiles(args.directory)

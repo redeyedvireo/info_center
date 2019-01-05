cd619eedf0270474023012e25cfbb44fe676c970
Jeff <abc@abc.com>
2019-01-05 13:36:53 -0500

    Move color constants to a central place.

---
+++ ui_colors.py
@@ -0,0 +1,11 @@
+# ui_colors.py
+#
+# Provides a central place to store color values.
+
+class UiColors:
+    WHITE = 255, 255, 255
+    GRAY = 80, 80, 80
+    GREEN = 0, 255, 0
+    BLACK = 0, 0, 0
+    BLUE = 0, 0, 255
+    RED = 255, 0, 0
